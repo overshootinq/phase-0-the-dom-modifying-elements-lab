e08e749f58602f32fa96f71afff2cbb34a99ad76
Brennan McCandliss <osmedia1220@gmail.com>
2021-08-24 15:29:24 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1> Brennan is the champion <h1>"
